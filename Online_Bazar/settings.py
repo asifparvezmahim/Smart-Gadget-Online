5a671db20c90ae268981dfb340ae319b785834f0
Md.Asif Parvez Mahim <100875772+asifparvezmahim@users.noreply.github.com>
2024-04-12 10:00:32 +0600

    env setup

--- Online_Bazar/settings.py
+++ Online_Bazar/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,5 +155,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "asifmahim58@gmail.com"
-EMAIL_HOST_PASSWORD = "gjjuwtosqdnqrvvy"
+EMAIL_HOST_USER = env("EMAIL")  #
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")  #
