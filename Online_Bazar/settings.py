58ea1b13d9caf040a475867c2d06cec9c2c55766
Md.Asif Parvez Mahim <100875772+asifparvezmahim@users.noreply.github.com>
2024-04-12 14:49:33 +0600

    auth faci

--- Online_Bazar/settings.py
+++ Online_Bazar/settings.py
@@ -140,7 +140,7 @@ MEDIA_URL = "/images/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     BASE_DIR / "Home" / "static",
