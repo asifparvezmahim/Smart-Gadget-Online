332e850d6a749685b16fd76f7f91d3a7aaee0a8b
Md.Asif Parvez Mahim <100875772+asifparvezmahim@users.noreply.github.com>
2024-04-17 11:42:00 +0600

    home_first

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class SliderImage(models.Model):
+    image = models.ImageField(upload_to="Home/banner_images/")
+
+    def __str__(self) -> str:
+        return f"{self.id} banner_image"
--- Home/views.py
+++ Home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from product.models import product_info
+from .models import SliderImage
+from product.models import product_info
 
 
 # Create your views here.
 def home(request):
     prd = product_info.objects.all()
-    return render(request, "home.html", {"prds": prd})
+    images = SliderImage.objects.all()
+    demos = product_info.objects.all()[:5]
+    return render(request, "home.html", {"prds": prd, "images": images, "demos": demos})
