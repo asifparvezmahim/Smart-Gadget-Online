9cf44d21ecd427397a4c61615d7df50fe06a5818
Md.Asif Parvez Mahim <100875772+asifparvezmahim@users.noreply.github.com>
2024-04-17 16:21:25 +0600

    home commit

--- Home/urls.py
+++ Home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="homepage"),
+    path("load_all_from/home/", views.load_all, name="load_all"),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from product.models import product_info
 from .models import SliderImage
 from product.models import product_info
@@ -10,3 +10,10 @@ def home(request):
     images = SliderImage.objects.all()
     demos = product_info.objects.all()[:5]
     return render(request, "home.html", {"prds": prd, "images": images, "demos": demos})
+
+
+def load_all(request):
+    if request.user.is_authenticated:
+        return redirect("display_products")
+    else:
+        return redirect("user_login")
